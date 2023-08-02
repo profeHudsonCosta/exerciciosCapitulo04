dcc1cd2008d52bed13b4d07b5bf4bdba56e69829
Prof. Hudson Costa <102161609+profeHudsonCosta@users.noreply.github.com>
2023-08-02 18:19:36 -0300

    Create script4.14.py

---
+++ script4.14.py
@@ -0,0 +1,12 @@
+Matriz =[]
+
+N = int(input("Entre com o número de linhas da matriz: "))
+M = int(input("Entre com o número de colunas da matriz: "))
+
+for i in range(0,N):
+    Matriz.append([])
+    for j in range(0,M):
+        el = int(input("Entre com o elemento da linha " + str(i+1) + " e coluna " + str(j + 1) + ": "))
+        Matriz[i].append(el)
+
+print(Matriz)
