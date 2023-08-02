021d0b426be3a2a7a17ad8feeadc76de556eef8c
Prof. Hudson Costa <102161609+profeHudsonCosta@users.noreply.github.com>
2023-08-02 18:29:37 -0300

    Create exercicio4.16.py
    Continuando o exercício anterior, faça um script que lê um ponto inicial, e lê um conjunto de pontos de uma rota. Quando o usuário digitar -1, o script mostra a distância total percorrida e finaliza.

---
+++ exercicio4.16.py
@@ -0,0 +1,17 @@
+dist = {1:{2:1, 3:2, 4:3, 5:4}, 
+        2:{1:2, 5:3},
+        3:{1:2, 4:2},
+        4:{1:3, 5:1, 3:2},
+        5:{1:4, 4:1, 2:3}}
+
+print("A distância entre 5 e 4 é ", dist[5][4])
+
+somaDist = 0
+flag = 1
+while flag != -1:
+    p1 = int(input("Informe o 1o ponto: "))
+    p2 = int(input("Informe o 2o ponto: "))
+    somaDist = somaDist + dist[p1][p2]
+    flag = int(input("Digite -1 para sair: "))
+
+print("A distância percorrida é: ", somaDist)
